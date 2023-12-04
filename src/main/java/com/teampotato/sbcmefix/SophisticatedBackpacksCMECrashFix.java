5e8300a8613a23db57f41d6c49fa9131b5390aca
Kasualix <670067575@qq.com>
2023-12-04 18:07:39 +0800

    Initial commit

---
+++ src/main/java/com/teampotato/sbcmefix/SophisticatedBackpacksCMECrashFix.java
@@ -0,0 +1,8 @@
+package com.teampotato.sbcmefix;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SophisticatedBackpacksCMECrashFix.MOD_ID)
+public class SophisticatedBackpacksCMECrashFix {
+    public static final String MOD_ID = "sbcmefix";
+}
